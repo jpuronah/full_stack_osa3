6f0fd110051526f0b7cb6dcb34c88d5b745faef7
jpuronah <jpuronah@student.hive.fi>
2023-07-22 11:02:04 +0300

    Part 3 DONE

--- index.js
+++ index.js
@@ -1,134 +1,133 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const mongoose = require('mongoose')
+//const mongoose = require('mongoose')
 const app = express()
-app.use(express.json());
+app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
-const Person = require('./models/person');
-const { ObjectId } = require('mongodb');
+const Person = require('./models/person')
+//const { ObjectId } = require('mongodb')
 //const Person = mongoose.model('Person', personSchema)
 //const Person = require('./mongo')
 //const Person = mongoose.model('Person', personSchema)
 
-morgan.token('body', (req) => JSON.stringify(req.body));
+morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(function (tokens, req, res) {
-	return [
-	  tokens.method(req, res),
-	  tokens.url(req, res),
-	  tokens.status(req, res),
-	  tokens.res(req, res, 'content-length'), '-',
-	  tokens['response-time'](req, res), 'ms',
-	  tokens['body'](req, res)
-	].join(' ')
-  }))
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens['body'](req, res)
+  ].join(' ')
+}))
 
 app.get('/api/persons/', (request, response, next) => {
-	//console.log(Person)
-	Person.find({})
-		.then(persons => {
-			response.json(persons)
-		})
-		.catch(error => next(new Error('Error finding persons')))
-});
-
-app.get('/info', (request, response, next) => {
-	Person.countDocuments({})
-		.then(personCount => {
-			const time = new Date();
-			response.send(`
-				<div>
-						<p>Phonebook has info for ${personCount} people</p>
-						<p>${time}</p>
-				</div>`);	
-		})
-		.catch(error => next(new Error('Error in counting the documents')))
-});
-
-app.get('/api/persons/:id', (request, response, next) => {
-	//const id = Number(request.params.id)
-	const id = request.params.id
-	Person.findById(id)
-		.then(person => {
-			if (person) {
-				response.json(person)
-			} else {
-			response.status(404).end();
-			}
-		})
-		.catch(error => next(new Error('malformatted id')))
-});
-
-app.delete('/api/persons/:id', (request, response, next) => {
-	const id = request.params.id
-
-	Person.findByIdAndRemove(id)
-		.then (() => {
-			response.status(204).end();
-		})
-		.catch(error => next(new Error('malformatted id (delete)')))
+  //console.log(Person)
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(() => next(new Error('Error finding persons')))
 })
 
+app.get('/info', (request, response, next) => {
+  Person.countDocuments({})
+    .then(personCount => {
+      const time = new Date()
+      response.send(`
+        <div>
+      	    <p>Phonebook has info for ${personCount} people</p>
+      	    <p>${time}</p>
+        </div>`)
+    })
+    .catch(() => next(new Error('Error in counting the documents')))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+  //const id = Number(request.params.id)
+  const id = request.params.id
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(() => next(new Error('malformatted id')))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+  
+  Person.findByIdAndRemove(id)
+    .then (() => {
+      response.status(204).end()
+    })
+    .catch(() => next(new Error('malformatted id (delete)')))
+})  
+
 app.post('/api/persons', (request, response, next) => {
-	const body = request.body
-	/*if (!body.name || !body.number) {
-		return next(new Error('Name or number missing'))
-	}*/
-	Person.findOne({name: body.name})
-		.then(personExists => {
-			if (personExists) {
-				return next(new Error('name must be unique'))
-			}
-			const person = new Person({
-				name: body.name,
-				number: body.number
-			});
-			person.save()
-				.then(savedPerson => {
-					response.json(savedPerson);
-				})
-				.catch(error => next(error))
-				//.catch(error => next(new Error('Error adding new person')))
-		})
-		.catch(error => next(new Error('error in person exist? (creating new)')))
+  const body = request.body
+  /*if (!body.name || !body.number) {
+  	return next(new Error('Name or number missing'))
+  }*/
+  Person.findOne({name: body.name})
+    .then(personExists => {
+      if (personExists) {
+        return next(new Error('name must be unique'))
+      }
+      const person = new Person({
+        name: body.name,
+        number: body.number
+      })
+      person.save()
+        .then(savedPerson => {
+          response.json(savedPerson)
+        })
+        .catch(error => next(error))
+        //.catch(() => next(new Error('Error adding new person')))
+    })
+    .catch(() => next(new Error('error in person exist? (creating new)')))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-	const id = request.params.id
-	const updatedPerson = request.body
-	Person.findByIdAndUpdate(id, updatedPerson, {new: true})
-		.then(updatedPerson => {
-			if (updatedPerson) {
-				response.json(updatedPerson)
-			}
-			else {
-				response.status(404).end()
-			}
-		})
-		.catch(error => next(new Error('error updating person')))
+  const id = request.params.id
+  const updatedPerson = request.body
+  Person.findByIdAndUpdate(id, updatedPerson, {new: true})
+    .then(updatedPerson => {
+      if (updatedPerson) {
+        response.json(updatedPerson)
+      }
+      else {
+        response.status(404).end()
+      }
+    })
+    .catch(() => next(new Error('error updating person')))
 })
 
 const errorHandler = (error, request, response, next) => {
-	let message = error.message
-	//console.log("ERROR HANDLER")
-	//console.log(error.message)
-	if (error.name === 'CastError') {
-		//console.log("cast")
-		return response.status(400).send({ error: 'malformatted id' })
-	
-	  } else if (error.name === 'ValidationError') {
-		//console.log("VALIDATIONERROR", error.message)
-		return response.status(400).json({ error: message })
-	  }
-	if (message) {
-		//console.log("psapaap")
-		//console.log("Error: ", message)
-		return response.status(400).json({message})
-	}
-	next(error)
+  let message = error.message
+  //console.log("ERROR HANDLER")
+  //console.log(error.message)
+  if (error.name === 'CastError') {
+  //console.log("cast")
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    //console.log("VALIDATIONERROR", error.message)
+    return response.status(400).json({ error: message })
+  }
+  if (message) {
+    //console.log("psapaap")
+    //console.log("Error: ", message)
+    return response.status(400).json({message})
+  }
+  next(error)
 }
 
 app.use(errorHandler)

6584095eee59d9e0f5bbe0c63d88cc284bda9664
jpuronah <jpuronah@student.hive.fi>
2023-07-22 17:45:38 +0300

    Models added, a bit late

---
+++ models/person.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+//const url = MONGODB_URI
+console.log('connecting to', url)
+
+mongoose.connect(url)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
+
+const personSchema = new mongoose.Schema({
+  id: Number,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{3}-\d{4}/.test(v)
+      }
+    }
+  }
+})
+//const Person = mongoose.model('Person', personSchema)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
